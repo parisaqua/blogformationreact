00cf1a14900b3a413b8032c386462b0bc8328611
Frédéric BASTIAN <frederic@bastian.paris>
2020-01-23 08:52:29 +0100

    Etape 51

--- src/composants/PostModale/PostModale.js
+++ src/composants/PostModale/PostModale.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 
 import './PostModale.css'
+import axios from 'axios'
 
 class PostModale extends Component {
 
+    state = {
+        loadedPost : null
+    }
+
+    componentDidUpdate() {
+        if(this.props.id){
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                //console.log(response)
+                this.setState({
+                    loadedPost: response.data
+                })
+            })
+        }
+    }
+    
     render () {
 
         return (
-            this.props.id ? 
+            this.state.loadedPost ? 
                 <div className="PostComplet">
-                    <h1>Titre</h1>
-                    <p>Contenu</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p> {this.state.loadedPost.body} </p>
             
                     <button className="btn btn-danger my-3 btnPost">Fermer</button>
                 
